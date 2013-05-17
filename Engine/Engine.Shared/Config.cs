8342698cbddb62c8338b289d9b6126b0b080df9d
FTWinston <ftwinston@gmail.com>
2013-05-17 15:32:11 +0100

    Added Yaml-based config system that doesn't expose the underlying structure. Also added a unit testing project.

---
+++ Engine/Engine.Shared/Config.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using YamlDotNet.RepresentationModel;
+using System.IO;
+
+namespace FTW.Engine.Shared
+{
+    public class Config
+    {
+        public static Config ReadFile(string path)
+        {
+            if ( !File.Exists(path) )
+                return null; // how should we fail?
+
+            var yaml = new YamlStream();
+            using ( StreamReader reader = File.OpenText(path) )
+            {
+                yaml.Load(reader);
+            }
+
+            YamlMappingNode mapping = yaml.Documents[0].RootNode as YamlMappingNode;
+            return AddToConfig(mapping, null, string.Empty);
+        }
+
+        private static Config AddToConfig(YamlNode yamlNode, Config parent, string nodeName)
+        {
+            Config node = new Config(nodeName);
+            if (yamlNode is YamlMappingNode)
+            {
+                YamlMappingNode mapping = yamlNode as YamlMappingNode;
+                node.Children = new List<Config>();
+                foreach (var child in mapping.Children)
+#if DEBUG
+                    if (child.Key is YamlScalarNode)
+#endif
+                        AddToConfig(child.Value, node, (child.Key as YamlScalarNode).Value);
+#if DEBUG
+                    else // going on the assumption that a Key is *always* a string. Can't wrap my head around how it wouldn't be.
+                        throw new InvalidCastException("YamlMappingNode's key is NOT a YamlScalarNode: " + child.Key.GetType().FullName);
+#endif
+            }
+            else if (yamlNode is YamlSequenceNode)
+            {
+                YamlSequenceNode sequence = yamlNode as YamlSequenceNode;
+                node.Children = new List<Config>();
+                foreach( var child in sequence.Children )
+                    AddToConfig(child, node, child.Tag);
+            }
+            else if (yamlNode is YamlScalarNode)
+            {
+                YamlScalarNode scalar = yamlNode as YamlScalarNode;
+                node.Value = scalar.Value;
+            }
+            else
+                throw new InvalidCastException("Unrecognised YamlNode type: " + yamlNode.GetType().FullName);
+            
+            if ( parent != null )
+                parent.Children.Add(node);
+            return node;
+        }
+
+
+        internal Config(string name) { Name = name; Value = null; Children = null; }
+        public string Name { get; internal set; }
+        public string Value { get; internal set; }
+        public List<Config> Children { get; internal set; }
+
+        public bool HasValue { get { return Value != null; } }
+        public bool HasChildren { get { return Children != null && Children.Count > 0;} }
+
+        public Config Find(string name)
+        {
+            if (!HasChildren)
+                return null;
+            
+            foreach (Config child in Children)
+                if (child.Name == name)
+                    return child;
+
+            return null;
+        }
+    }
+}
---
+++ Tools/UnitTests/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FTW.Engine.Shared;
+
+namespace UnitTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Config config = Config.ReadFile("sample.yml");
+            TestResult("Config reader",
+                () => config.Find("ship-to").Find("state").Value == "KS",
+                () => config.Find("items").Children[1].Find("quantity").Value == "1"
+                );
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        delegate bool testFunc();
+
+        static void TestResult(string name, params testFunc[] tests)
+        {
+            Console.WriteLine(name);
+            int failures = 0;
+
+            for (int i = 0; i < tests.Length; i++)
+            {
+                Console.Write("  ");
+                Console.Write(i + 1);
+                Console.Write("/");
+                Console.Write(tests.Length);
+                Console.Write(" ... ");
+
+                if (tests[i]())
+                    Console.WriteLine("passed");
+                else
+                {
+                    Console.WriteLine("FAILED");
+                    failures++;
+                }
+            }
+
+            if (failures == 0)
+                Console.WriteLine(name + " passed all tests");
+            else
+            {
+                Console.Error.Write(name);
+                Console.Error.Write(" failed ");
+                Console.Error.Write(failures);
+                Console.Error.Write("/");
+                Console.Error.Write(tests.Length);
+                Console.Error.WriteLine(" tests");
+            }
+            Console.WriteLine();
+        }
+    }
+}
