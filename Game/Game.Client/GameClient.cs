877e9d2117a071cc1c93d78dbf3a8aecd6f3429d
FTWinston <ftwinston@gmail.com>
2013-05-31 00:45:15 +0100

    The console now shows/hides with the backtick or tilde keys (` or ~).
    Next up, the console should have a background and be able to scroll and stuff.

--- Game/Game.Client/GameClient.cs
+++ Game/Game.Client/GameClient.cs
@@ -37,7 +37,9 @@ namespace Game.Client
             return config;
         }
 
-        Text loading, console;
+        Text loading, console, game;
+        public bool ShowConsole;
+
         public void SetupDisplay()
         {
             font = new Font("Resources/arial.ttf");
@@ -49,6 +51,10 @@ namespace Game.Client
             console = new Text("", font, 18);
             console.Color = Color.White;
             console.Position = new Vector2f(8, 8);
+
+            game = new Text("This is the game. Blah blah.", font, 48);
+            game.Color = Color.Yellow;
+            game.Position = new Vector2f(window.Size.X / 2 - game.GetLocalBounds().Width / 2, window.Size.Y / 2 - game.GetLocalBounds().Height / 2);
 
             TextStreamWriter tw = new TextStreamWriter(console);
             Console.SetOut(tw);
@@ -95,7 +101,12 @@ namespace Game.Client
             Update();
 
             if (FullyConnected)
-                target.Draw(console);
+            {
+                if (ShowConsole)
+                    target.Draw(console);
+                else
+                    target.Draw(game);
+            }
             else
                 target.Draw(loading);
         }
@@ -104,7 +115,10 @@ namespace Game.Client
         {
             if (e.Code == Keyboard.Key.Escape)
             {
-                window.CurrentMenu = window.InGameMenu;
+                if (ShowConsole)
+                    ShowConsole = false;
+                else
+                    window.CurrentMenu = window.InGameMenu;
                 return;
             }
 
@@ -112,7 +126,12 @@ namespace Game.Client
         }
 
         public void KeyReleased(KeyEventArgs e) { }
-        public void TextEntered(TextEventArgs e) { }
+        public void TextEntered(TextEventArgs e)
+        {
+            // using TextEntered because there's no KeyCode for using backtick in KeyPressed
+            if ( e.Unicode == "`" || e.Unicode == "~" )
+                ShowConsole = !ShowConsole;
+        }
         public void MousePressed(MouseButtonEventArgs e) { }
         public void MouseReleased(MouseButtonEventArgs e) { }
         public void MouseMoved(MouseMoveEventArgs e) { }
